5db4b553ac729d17c1273ad995801aa26d8ce3b1
Nilay611 <nilay.saxena.06@gmail.com>
2025-03-08 21:32:23 +0530

    FEAT: Added navbar and all tests working as expected. Removed coverage folder from commit and added it to gitignore.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Navbar } from "./components/Navbar/Navbar";
+
 function App() {
-  return <>GSynergy Data Viewer App</>;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 }
 
 export default App;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,18 @@
 import { FC } from "react"; // React functional component, using it to cast navbar to type react functional component
 
 export const Navbar: FC = () => {
-  return <div></div>;
+  return (
+    <div
+      data-testid={"navbar"}
+      className={"flex flex-row justify-between items-center px-4 py-2"}
+    >
+      <div className={"w-[150px]"}>
+        <img src={"/assets/images/gsynergy-logo.svg"} alt={"GSynergy Logo"} />
+      </div>
+      <div>
+        <h1 className={"text-4xl font-semibold"}>Data Viewer App</h1>
+      </div>
+      <div>User</div>
+    </div>
+  );
 };
